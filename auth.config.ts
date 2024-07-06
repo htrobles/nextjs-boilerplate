44af8062a827b18d4826401ac11c4ae47159f474
htrobles <hjt.robles@gmail.com>
2024-07-06 17:13:10 -0400

    include facebook login

--- auth.config.ts
+++ auth.config.ts
@@ -1,11 +1,18 @@
+import bcrypt from 'bcryptjs';
 import type { NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
+import Github from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
+import Facebook from 'next-auth/providers/facebook';
+
 import { LoginSchema } from './schemas';
 import { getUserByEmail } from './data/user';
-import bcrypt from 'bcryptjs';
 
 export default {
   providers: [
+    Facebook,
+    Github,
+    Google,
     Credentials({
       authorize: async (credentials) => {
         const validatedFields = LoginSchema.safeParse(credentials);
