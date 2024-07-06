34c29f496f41736df24dd73b3e0839d30982c2c4
htrobles <hjt.robles@gmail.com>
2024-07-06 00:38:24 -0400

    update login logic

--- actions/login.ts
+++ actions/login.ts
@@ -1,14 +1,42 @@
 'use server';
 
+import { signIn } from '@/auth';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 import { LoginSchema } from '@/schemas';
+import { AuthError } from 'next-auth';
 import { z } from 'zod';
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
 
-  if (!validatedFields) {
+  if (!validatedFields.success) {
     return { error: 'Login failed' };
   }
 
-  return { success: 'Login success' };
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirect: true,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+
+    return { success: 'Login succesful' };
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return { error: 'Invalid credentials' };
+        default:
+          return { error: 'Something went wrong.' };
+      }
+    }
+
+    /**
+     * Do not remove this. Redirect does not work if throw error is not present.
+     */
+    throw error;
+  }
 };
